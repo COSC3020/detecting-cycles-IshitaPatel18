957090b1180bb3a910f1dd4941ebe92897600069
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-04-10 16:06:24 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,34 @@
-function hasCycle(graph) {
-    return false;
+function hasCycle(graph) 
+{
+  let visitedNodes = [];
+  for (let i = 0; i < graph.length; i++) 
+  {
+    if (checkCycle(graph, i, i, visitedNodes)) 
+    {
+      return true;
+    }
+    visitedNodes = [];
+  }
+  return false;
+}
+
+function checkCycle(graph, startNode, prevNode, visitedNodes) 
+{
+  visitedNodes.push(startNode);
+  for (let i = 0; i < graph[startNode].length; i++) 
+  {
+    let node = graph[startNode][i];
+    if (node != prevNode) 
+    {
+      if (visitedNodes.includes(node)) 
+      {
+        return true;
+      }
+      if (checkCycle(graph, node, startNode, visitedNodes)) 
+      {
+        return true;
+      }
+    }
+  }
+  return false;
 }
